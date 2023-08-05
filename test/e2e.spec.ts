6e62b486077ac2ac676fe86b5d34e9e6dcec72e4
Dolan Murvihill <dolan@directdemocracysolutions.com>
2023-08-04 21:01:07 -0700

    Get ESM working

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -28,75 +28,78 @@ const pileDirPath = path.join(__dirname, 'e2e-test-piles');
 const numPiles = 3;
 
 describe('shuffle', () => {
-  it('should properly shuffle a CSV', async () => {
-    let countIn = 0;
-    let countOut = 0;
-    let resolveHeaders: (headers: string[]) => void;
+  it(
+    'should properly shuffle a CSV',
+    async () => {
+      let countIn = 0;
+      let countOut = 0;
+      let resolveHeaders: (headers: string[]) => void;
 
-    const headers = new Promise<string[]>((resolve) => {
-      resolveHeaders = resolve;
-    });
+      const headers = new Promise<string[]>((resolve) => {
+        resolveHeaders = resolve;
+      });
 
-    function incrCountIn<T>(record: T): T {
-      countIn++;
-      return record;
-    }
+      function incrCountIn<T>(record: T): T {
+        countIn++;
+        return record;
+      }
 
-    function incrCountOut<T>(record: T): T {
-      countOut++;
-      return record;
-    }
+      function incrCountOut<T>(record: T): T {
+        countOut++;
+        return record;
+      }
 
-    function pipelineCompleteHandler(
-      name: string,
-      resolve?: (r?: any) => void,
-      reject?: (e: Error) => void,
-    ): (e?: ErrnoException | null) => void {
-      return (e?: ErrnoException | null): void => {
-        if (e !== null && e !== undefined) {
-          console.error(e);
-          if (reject !== undefined) {
-            reject(e);
+      function pipelineCompleteHandler(
+        name: string,
+        resolve?: (r?: any) => void,
+        reject?: (e: Error) => void,
+      ): (e?: ErrnoException | null) => void {
+        return (e?: ErrnoException | null): void => {
+          if (e !== null && e !== undefined) {
+            console.error(e);
+            if (reject !== undefined) {
+              reject(e);
+            }
+          } else if (resolve !== undefined) {
+            resolve();
           }
-        } else if (resolve !== undefined) {
-          resolve();
-        }
-      };
-    }
+        };
+      }
 
-    const inStream = pipeline(
-      fs.createReadStream(path.join(__dirname, inFileName)),
-      csv_read.parse(parserOptions).once('data', (record) => {
-        resolveHeaders(Object.keys(record));
-      }),
-      new LambdaTransform((o: object) => JSON.stringify(o)),
-      new LambdaTransform(incrCountIn),
-      pipelineCompleteHandler('read'),
-    );
+      const inStream = pipeline(
+        fs.createReadStream(path.join(__dirname, inFileName)),
+        csv_read.parse(parserOptions).once('data', (record) => {
+          resolveHeaders(Object.keys(record));
+        }),
+        new LambdaTransform((o: object) => JSON.stringify(o)),
+        new LambdaTransform(incrCountIn),
+        pipelineCompleteHandler('read'),
+      );
 
-    const shuffled = await shuffle(
-      new StreamToAsyncIterator(inStream),
-      numPiles,
-      pileDirPath,
-    );
+      const shuffled = await shuffle(
+        new StreamToAsyncIterator(inStream),
+        numPiles,
+        pileDirPath,
+      );
 
-    const outStream = headers.then(
-      (h: string[]) =>
-        new Promise((resolve, reject) =>
-          pipeline(
-            Readable.from(shuffled),
-            new LambdaTransform(incrCountOut),
-            new LambdaTransform((line: string) => JSON.parse(line)),
-            csv_write.stringify({ columns: h, ...writeOptions }),
-            fs.createWriteStream(path.join(__dirname, outFileName)),
-            pipelineCompleteHandler('write', resolve, reject),
+      const outStream = headers.then(
+        (h: string[]) =>
+          new Promise((resolve, reject) =>
+            pipeline(
+              Readable.from(shuffled),
+              new LambdaTransform(incrCountOut),
+              new LambdaTransform((line: string) => JSON.parse(line)),
+              csv_write.stringify({ columns: h, ...writeOptions }),
+              fs.createWriteStream(path.join(__dirname, outFileName)),
+              pipelineCompleteHandler('write', resolve, reject),
+            ),
           ),
-        ),
-    );
+      );
 
-    await outStream;
-    expect(countOut).toEqual(countIn);
-  });
+      await outStream;
+      expect(countOut).toEqual(countIn);
+    },
+  );
 });
 
 class LambdaTransform<T, U> extends Transform {
