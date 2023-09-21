0b152e35ac1ef417b9846ba102d79038613b44b3
Dolan Murvihill <dolan@directdemocracysolutions.com>
2023-09-21 13:54:56 -0700

    Fix import name collision in CI

--- __mocks__/fs/promises/index.ts
+++ __mocks__/fs/promises/index.ts
@@ -1,13 +1,13 @@
-const { fs } = require('memfs');
+const promises = require('memfs').promises;
 
 module.exports = {
-  ...fs.promises,
+  ...promises,
   open: jest.fn()
-    .mockImplementation((...args) => fs.promises.open(...args)),
+    .mockImplementation((...args) => promises.open(...args)),
   readFile: jest.fn()
-    .mockImplementation((...args) => fs.promises.readFile(...args)),
+    .mockImplementation((...args) => promises.readFile(...args)),
   unlink: jest.fn()
-    .mockImplementation((...args) => fs.promises.unlink(...args)),
+    .mockImplementation((...args) => promises.unlink(...args)),
   rm: jest.fn()
-    .mockImplementation((...args) => fs.promises.rm(...args)),
+    .mockImplementation((...args) => promises.rm(...args)),
 };
